789c0e5dc9aebdc354a3dadd8db57c681b2d3470
Christian <cbt_97@hotmail.com>
2017-11-15 17:53:25 +0100

    Sending message to slack

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -71,16 +72,24 @@ func main() {
 }
 
 func testhandler(w http.ResponseWriter, r *http.Request) {
-	/*err := r.ParseForm()
+	//Getting message from slack:
+	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Error parsing", http.StatusTeapot)
 	}
 	form := r.Form
 
-	form.Get("text")*/
+	form.Get("text")
+	fmt.Fprintln(w, form)
 
+	//sending message to dialogflow:
+
+	//sending message to other Application
+
+	//Use correct body for sending message to slack:
+
+	//Sending message to slack:
 	webhook := "https://hooks.slack.com/services/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"
-
 	var body = []byte(`{"text":"Whats up?"}`)
 	req, err := http.NewRequest("POST", webhook, bytes.NewBuffer(body))
 	if err != nil {
@@ -89,54 +98,8 @@ func testhandler(w http.ResponseWriter, r *http.Request) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		http.Error(w, "Error parsing", http.StatusTeapot)
+		http.Error(w, "Error posting", http.StatusTeapot)
 	}
 	defer resp.Body.Close()
 
-	//Get incoming post Request with json values from dialogflow
-	//if r.Method == "POST" {
-	/*	decoder := json.NewDecoder(r.Body)
-		var f IncomingPost
-
-		err := decoder.Decode(&f)
-		if err != nil {
-			http.Error(w, "Error decoding post request for average", http.StatusBadRequest)
-		} else {
-	*/
-
-	//Edit the values in order to send to assignment2 application
-
-	//Send response back to dialogflow with correct values in json format.
-	/*var test = outgoingPost{}
-	test.Contexts[0] = "shop"
-	test.Lang = "en"
-	test.Query = "Fuck you!"
-	test.SessionID = "dgfch43ret"
-	test.TimeZone = "America/New_York"
-
-	b, err := json.Marshal(test)
-	if err != nil {
-		fmt.Fprintln(w, "error marshalling")
-	}
-	webhook := "https://hooks.slack.com/services/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"
-	var body = []byte(b)
-	req, err := http.NewRequest("POST", webhook, bytes.NewBuffer(body))
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Content-Type", "Application/json")
-	req.Header.Set("Authorization", "Bearerd439bdaebe064bf5b1d910aef3fcf510")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	fmt.Fprintln(w, "sending message")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	*/
-	//}
-	/*} else {
-		http.Error(w, "Invalid request type", http.StatusMethodNotAllowed)
-	}*/
 }
