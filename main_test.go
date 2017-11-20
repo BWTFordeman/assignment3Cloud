e5eae3a1221adc0a5f4af7e48bf499dc1edc8308
Christian <cbt_97@hotmail.com>
2017-11-20 02:14:25 +0100

    Tests

--- main_test.go
+++ main_test.go
@@ -126,23 +126,3 @@ func TestPostRequest6(t *testing.T) {
 
 	postRequest("https://evil-barrow-41137.herok", rr, req)
 }
-
-func TestPostRequest7(t *testing.T) {
-	var data = LatestRequest{
-		BaseCurrency:   "",
-		TargetCurrency: "",
-	}
-	m, err := json.Marshal(data)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req, err := http.NewRequest("POST", "/dialogflow", ioutil.NopCloser(strings.NewReader(string(m))))
-	req.Header.Add("Content-Type", "application/json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	rr := httptest.NewRecorder()
-
-	postRequest("https://evil-barrow-41137.herokuapp.com/assignment2/", rr, req)
-}
